588e6ff7e84d2e759dfdc9092278e90c3ca40925
Jonathan Sanfer <sanferjonathan88@gmail.com>
2017-10-23 21:43:59 +0100

    Initial commit

---
+++ test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int add (int a, int b) {
+	int c;
+	c = a + b;
+	return c;
+}
+
+int sub (int a, int b) {
+	int c;
+	c = a - b;
+	return c;
+}
+
+int main () {
+	int a, b, c, d;
+	printf("hello, give two numbers!!\n");
+	scanf("%d", &a);
+	scanf("%d", &b);
+	c = add(a, b);
+	d = sub(a, b);
+	printf("a + b = %d och a - b = %d\n", c, d);
+	return 0;
+}
